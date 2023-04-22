1fe28d8c41e81f99dd7df3c12d024b5409d2e944
Water.Li <lizongbin@hotmail.com>
2023-04-22 21:04:56 +0800

    finished types

--- system/types.go
+++ system/types.go
@@ -1,15 +1,39 @@
 package system
 
-import "github.com/codebdy/entify/shared"
+import (
+	"time"
+
+	"github.com/codebdy/entify/model/meta"
+	"github.com/codebdy/entify/shared"
+)
 
 type Meta struct {
-	Id shared.ID `json:"id"`
+	Id               shared.ID    `json:"id"`
+	Name             string       `json:"name"`
+	Content          meta.UMLMeta `json:"content"`
+	PublishedContent meta.UMLMeta `json:"publishedContent"`
+	PublishedAt      time.Time    `json:"publishedAt"`
+	CreatedAt        time.Time    `json:"createdAt"`
+	UpdatedAt        time.Time    `json:"updatedAt"`
 }
 
 type App struct {
-	Id shared.ID `json:"id"`
+	Id        shared.ID `json:"id"`
+	Name      string    `json:"name"`
+	Title     string    `json:"title"`
+	ImageUrl  string    `json:"imageUrl"`
+	MetaId    shared.ID `json:"metaId"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type Service struct {
-	Id shared.ID `json:"id"`
+	Id        shared.ID `json:"id"`
+	Name      string    `json:"name"`
+	Title     string    `json:"title"`
+	ImageUrl  string    `json:"imageUrl"`
+	MetaId    shared.ID `json:"metaId"`
+	IsSystem  bool      `json:"isSystem"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
