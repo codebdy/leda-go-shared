14e1bd972a6a1055d2a200da01e49922db85bdd3
Water.Li <lizongbin@hotmail.com>
2023-04-22 21:54:29 +0800

    Update ledasdk.go

--- ledasdk.go
+++ ledasdk.go
@@ -2,9 +2,22 @@ package ledasdk
 
 import (
 	"github.com/codebdy/entify/db"
-	"github.com/codebdy/entify/model/meta"
+	"github.com/codebdy/entify/shared"
+	"github.com/codebdy/leda-service-sdk/system"
 )
 
-func GetMata(name string, dbConfig db.DbConfig) (*meta.UMLMeta, error) {
+func GetAppMata(appName string, dbConfig db.DbConfig) (*system.Meta, error) {
+	return nil, nil
+}
+
+func GetServiceMata(serviceName string, dbConfig db.DbConfig) (*system.Meta, error) {
+	return nil, nil
+}
+
+func GetAppMataById(appId shared.ID, dbConfig db.DbConfig) (*system.Meta, error) {
+	return nil, nil
+}
+
+func GetServiceMataById(serviceId shared.ID, dbConfig db.DbConfig) (*system.Meta, error) {
 	return nil, nil
 }
