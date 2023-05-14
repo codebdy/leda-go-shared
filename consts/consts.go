05f741582a3f0f525c3196f3061fe6b6824e19eb
Water.Li <lizongbin@hotmail.com>
2023-05-14 18:29:26 +0800

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -1,5 +1,12 @@
 package consts
 
+const HEADER_LEDA_APPID = "leda-appid"
+const HEADER_LEDA_APPNAME = "leda-appname"
+const (
+	TOKEN         = "token"
+	AUTHORIZATION = "Authorization"
+	BEARER        = "Bearer "
+)
 const META_ENTITY_NAME = "Meta"
 const APP_ENTITY_NAME = "App"
 const SERVICE_ENTITY_NAME = "Service"
